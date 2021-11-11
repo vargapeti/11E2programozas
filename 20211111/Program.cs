3d9496ca363d4f2055ea144330677e9656a7de02
vargapeti <91724119+vargapeti@users.noreply.github.com>
2021-11-11 10:31:29 +0100

    Órai munka
    20211111

---
+++ 20211111/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _20211111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] beolvas = File.ReadAllLines("karakterek.txt", Encoding.Default);
+            int sorokszama = beolvas.Length;
+            int oszlopszam = beolvas[0].Split(' ').Length;
+            char[,] karakterek = new char[sorokszama, oszlopszam];
+            for (int i = 0; i < sorokszama; i++)
+            {
+                string[] darabok = beolvas[i].Split(' ');
+                for (int j = 0; j < oszlopszam; j++)
+                {
+                    karakterek[i, j] = char.Parse(darabok[j]);
+                }
+            }
+            Console.WriteLine("Elemek száma: {0}", karakterek.Length);
+            Console.WriteLine("Sorok száma: {0}", karakterek.GetLength(0));
+            Console.WriteLine("Oszlopok száma: {0}", karakterek.GetLength(1));
+
+            Console.WriteLine("Enter");
+            Console.ReadLine();
+        }
+    }
+}
